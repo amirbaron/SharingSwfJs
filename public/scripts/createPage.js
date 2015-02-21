0503cfb56be8358e0ee81d9feb02d05d04c6ff1f
amir <amir>
2015-02-21 15:37:12 +0200

    commit

--- public/scripts/createPage.js
+++ public/scripts/createPage.js
@@ -1,3 +1,9 @@
 /**
  * Created by amirbaron on 2/20/15.
  */
+
+Parse.initialize("BZ5XSiStXyIe6ukM9iuZwPg3rOeRz8l2yQDweSNv", "hWsKxXUSU6OxCnEL6jDEM1VAxwk1EWlClEWhwGqH");
+var Page = Parse.Object.extend("Page");
+var page = new Page();
+
+
