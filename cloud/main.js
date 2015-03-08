2d02f405a5422a2ccd395ab02ee490593bef68f8
amir <amir>
2015-03-08 21:11:44 +0200

    Added quiz upload

--- cloud/main.js
+++ cloud/main.js
@@ -5,3 +5,18 @@ require('cloud/app.js');
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello " + request.params.myName + "!");
 });
+
+Parse.Cloud.define("item", function(request, response) {
+  var AppPage = Parse.Object.extend("AppPage");
+  var query = new Parse.Query(AppPage);
+  query.get(request.params.id, {
+    success: function(page) {
+      response.success(page.get("page"));
+    },
+    error: function(object, error) {
+      response.error("Failed to find page with id: " + request.params.id);
+    }
+  });
+
+
+});
