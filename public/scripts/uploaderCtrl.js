fa294aab9c258d6ac5226015b195688b44fd1d5d
amir <amir>
2015-03-14 17:20:01 +0200

    introduced bower and site is more pretty now

--- public/scripts/uploaderCtrl.js
+++ public/scripts/uploaderCtrl.js
@@ -1,4 +1,4 @@
-app.controller("uploaderCtrl", function ($scope,$location) {
+app.controller("uploaderCtrl", function ($scope, $location) {
     $scope.message = "";
     $scope.numberOfChars = function () {
         return $scope.message.length;
@@ -16,41 +16,79 @@ app.controller("uploaderCtrl", function ($scope,$location) {
 
         appPage.save(null, {
             success: function (object) {
-                alert("Saved with parse id: " + object.id);
+                console.log("Saved with parse id: " + object.id);
+                $scope.clear();
+                $scope.$digest();
             },
             error: function (model, error) {
                 alert("Error" + error);
             }
         });
     };
-    $scope.getItemUrl = function(item){
-      return encodeURI($location.protocol()+"://"+$location.host()+"/item/"+item.objectId+"?title="+item.title+"&logoImg="+item.logoImg);
+    $scope.getItemUrl = function (item) {
+        return encodeURI($location.protocol() + "://" + $location.host() + "/item/" + item.objectId + "?title=" + item.title + "&logoImg=" + item.logoImg);
     };
 
-    $scope.searchParam={
-        "sortField":'title',
-        "ascending" :true
+    $scope.searchParam = {
+        "sortField": 'updatedAt',
+        "ascending": true
+    };
+
+    var myObjects = [];
+    $scope.getObjects = function () {
+
+        var AppPage = Parse.Object.extend("AppPage");
+        var query = new Parse.Query(AppPage);
+        query.find({
+            success: function (results) {
+                var newObjects = [];
+                // Do something with the returned Parse.Object values
+                for (var i = 0; i < results.length; i++) {
+                    var object = results[i];
+                    var pageJson = JSON.parse(object.get("page"));
+                    newObjects.push({
+                        'objectId': object.id,
+                        'title': object.get("title"),
+                        "previewImg": pageJson.previewImg,
+                        'updatedAt': object.updatedAt
+                    });
+
+                }
+                if(!angular.equals(newObjects, myObjects)){
+                    myObjects= newObjects;
+                    $scope.$digest();
+                }
+            },
+            error: function (error) {
+                alert("Error: " + error.code + " " + error.message);
+            }
+        });
+        return myObjects;
+    };
+
+
+    $scope.removeItem = function (guiItem) {
+        var AppPage = Parse.Object.extend("AppPage");
+        var query = new Parse.Query(AppPage);
+        query.get(guiItem.objectId, {
+            success: function (page) {
+                page.destroy({
+                    success: function (page) {
+                        console.log("deleted " + guiItem.objectId);
+                        $scope.$digest();
+                    },
+                    error: function (page, error) {
+                        console.log("failed to deleted " + guiItem.objectId)
+                    }
+                });
+            },
+            error: function (page, error) {
+                console.log("Failed to retrieve " + guiItem.objectId)
+            }
+        });
     }
 
 
-    var AppPage = Parse.Object.extend("AppPage");
-    var query = new Parse.Query(AppPage);
-
-    query.find({
-        success: function(results) {
-            $scope.objects = [];
-            // Do something with the returned Parse.Object values
-            for (var i = 0; i < results.length; i++) {
-                var object = results[i];
-                var pageJson = JSON.parse(object.get("page"));
-                $scope.objects.push({'objectId':object.id,'title':object.get("title"),"previewImg":pageJson.previewImg});
-            }
-            $scope.$apply();
-        },
-        error: function(error) {
-            alert("Error: " + error.code + " " + error.message);
-        }
-    });
 
 
 });
