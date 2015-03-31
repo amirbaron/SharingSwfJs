f569942e1f3ce294db95cf495d7c9a1718009f43
akiriati <akiriati@hotmail.com>
2015-03-31 21:41:52 +0300

    add activity in modal

--- public/scripts/controllers/itemViewerCtrl.js
+++ public/scripts/controllers/itemViewerCtrl.js
@@ -2,5 +2,17 @@ app.controller("itemViewerCtrl", function ($scope, $location,$modalInstance,item
     // Queries
     console.log("in itemViewerCtrl selected item is " + item.id);
     $scope.item=item;
+    $scope.selectedSlide = 0;
+    Parse.Object.extend({
+        className: "AppPage",
+        attrs: ['page', 'name', 'title', 'type'],
+        getPageObject: function () {
+            return angular.fromJson(this.getPage());
+        }
+    });
+
+    $scope.entityClicked = function() {
+        $scope.selectedSlide++;
+    }
 });
 
