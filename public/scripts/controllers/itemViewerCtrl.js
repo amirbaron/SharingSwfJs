616ef00a96be4b2c1c4bdc5f907fd77b1c40903c
akiriati <akiriati@hotmail.com>
2015-04-09 16:27:26 +0300

    bootstrap activity

--- public/scripts/controllers/itemViewerCtrl.js
+++ public/scripts/controllers/itemViewerCtrl.js
@@ -3,6 +3,8 @@ app.controller("itemViewerCtrl", function ($scope, $location,$modalInstance,item
     console.log("in itemViewerCtrl selected item is " + item.id);
     $scope.item=item;
     $scope.selectedSlide = 0;
+    $scope.page = $scope.item.getPageObject();
+    $scope.a = 0;
     Parse.Object.extend({
         className: "AppPage",
         attrs: ['page', 'name', 'title', 'type'],
@@ -11,8 +13,47 @@ app.controller("itemViewerCtrl", function ($scope, $location,$modalInstance,item
         }
     });
 
-    $scope.entityClicked = function() {
-        $scope.selectedSlide++;
+    $scope.entityClicked = function(entityIndex, itemSelected) {
+      //  $scope.selectedSlide++;
+        //$scope.page.slides[$scope.selectedSlide].entities[entityIndex];
+
+        setTimeout(function() {
+            $scope.selectedSlide++; $scope.$apply()}, 1000);
+
+        var selectedSlide = $scope.page.slides[$scope.selectedSlide];
+        var selectedEntity = selectedSlide.entities[entityIndex];
+
+        if (selectedEntity.points == 0)
+        {
+            selectedEntity.isFailed = true;
+            selectedSlide.isFailed = true;
+        }
+        else
+        {
+            selectedSlide.isSuccess = true;
+        }
+
+        selectedSlide.entities.forEach(
+            function handleEntity(entity)
+            {
+                if (entity.points > 0)
+                {
+                    entity.isSucess = true;
+                }
+                entity.isPressed = true;
+            }
+        );
+
+       // $scope.a++;
+       // slideVisibility='hideit';
     }
+
+
+
+    $scope.jumpToSlide = function(slideIndex) {
+        $scope.selectedSlide =  slideIndex; }
+
+
+
 });
 
