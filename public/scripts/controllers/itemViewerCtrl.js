abe40e96e670b2b0bebc19c956c72e32350f0fb9
akiriati <akiriati@hotmail.com>
2015-04-23 23:01:14 +0300

    view-edit mode

--- public/scripts/controllers/itemViewerCtrl.js
+++ public/scripts/controllers/itemViewerCtrl.js
@@ -6,7 +6,7 @@ app.controller("itemViewerCtrl", function ($scope, $location,$modalInstance,item
     $scope.item=item;
     $scope.selectedSlide = 0;
     $scope.page = $scope.item.getPageObject();
-    $scope.a = 0;
+    $scope.editMode = false;
     $scope.myUrl = '#'+$location.url();
 
     $scope.entityClicked = function(entityIndex, itemSelected) {
@@ -53,6 +53,7 @@ app.controller("itemViewerCtrl", function ($scope, $location,$modalInstance,item
         var fbpopup = window.open('https://www.facebook.com/sharer/sharer.php?u=quiz22.parseapp.com/i/'+item.id, "pop", "width=600, height=400, scrollbars=no");
     }
 
+    $scope
 
 });
 
