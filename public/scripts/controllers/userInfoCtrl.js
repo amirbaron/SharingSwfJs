56da8a5cb5dfd4f13160a6099c1bc0fe85da6fef
amir <amir>
2015-05-02 21:55:13 +0300

    Added user profile

--- public/scripts/controllers/userInfoCtrl.js
+++ public/scripts/controllers/userInfoCtrl.js
@@ -1,8 +1,11 @@
 app.controller("userInfoCtrl", function ($scope, $location, userInfoService) {
 
-    $scope.firstName= 'foo';
-    userInfoService.getFirstName().then(function(data) {
-        $scope.firstName = data;
+    userInfoService.getFBInfo().then(function(data) {
+        $scope.firstName = data.first_name;
+        $scope.lastName=data.last_name;
+        $scope.email=data.email;
+        $scope.imgUrl='//graph.facebook.com/'+data.id+'/picture?type=large';
+
     });
 
 
