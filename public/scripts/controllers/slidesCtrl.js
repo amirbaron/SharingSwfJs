5001b7e2d5053fd0c5c2bcc2a9a1d026e3c13bfe
akiriati <akiriati@hotmail.com>
2015-05-24 00:51:02 +0300

    remove slides logic

--- public/scripts/controllers/slidesCtrl.js
+++ public/scripts/controllers/slidesCtrl.js
@@ -84,6 +84,16 @@ app.controller("slidesCtrl", function ($scope, $location, item, loginService, $s
         });
     }
 
+    $scope.removeSlide = function()
+    {
+        if ($scope.page.slides.length <= 1)
+            return;
+
+        $scope.page.slides.splice($scope.selectedSlide,1);
+        if ($scope.selectedSlide >= $scope.page.slides.length)
+            $scope.selectedSlide = $scope.page.slides.length-1;
+    }
+
 
 
 });
